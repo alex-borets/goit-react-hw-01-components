06634cf1758e50bc18ddadce6cedc58b112ab40c
Alex Borets <alexborets2812@gmail.com>
2022-07-30 23:57:43 +0300

    HW ready

---
+++ src/components/FriendList/index.jsx
@@ -0,0 +1,38 @@
+import PropTypes from 'prop-types';
+import css from './FriendList.module.css';
+
+export const FriendList = ({ friends }) => {
+  return (
+    <ul className={css.friendList}>
+      {friends.map(({ avatar, name, isOnline, id }) => {
+        const statusColor = isOnline ? 'green' : 'red';
+        return (
+          <li className={css.item} key={id}>
+            <span
+              className={css.status}
+              style={{ backgroundColor: statusColor }}
+            ></span>
+            <img
+              className={css.avatar}
+              src={avatar}
+              alt="User avatar"
+              width="48"
+            />
+            <p className={css.name}>{name}</p>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      avatar: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+      id: PropTypes.number.isRequired,
+    })
+  ),
+};
